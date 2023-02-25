74b79f2552b5224c01606a6ccdd34888705d191a
tandica <williams.tandica@gmail.com>
2023-02-25 15:38:55 -0500

    set up backend db connection and add package files

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const app = express();
