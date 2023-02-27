51cd4ddca01ce17f840d1f143e9247943871adc7
tandica <williams.tandica@gmail.com>
2023-02-27 13:21:18 -0500

    add route for getting one query, add filtering by date functionality

--- backend/transactions/routes.js
+++ backend/transactions/routes.js
@@ -10,21 +10,89 @@ const transactionsRouter = express.Router();
 transactionsRouter.get(
   "/",
   expressAsyncHandler(async (req, res) => {
-    const getAllTransactions = await Transactions.insertMany(
-      transactionsData.transactions
-    );
+    // const { startDate, endDate } = req.query;
+    // const query = {};
+
+    // if (startDate && endDate) {
+    //   query.date = { $gte: new Date(startDate), $lte: new Date(endDate) };
+    // } else if (startDate) {
+    //   query.date = { $gte: new Date(startDate) };
+    // } else if (endDate) {
+    //   query.date = { $lte: new Date(endDate) };
+    // }
+
+    const { startDate, endDate } = req.query;
+
+    let query = {};
+    if (startDate && endDate) {
+      const start = new Date(startDate);
+      const end = new Date(endDate);
+
+      if (!isNaN(start.getTime()) && !isNaN(end.getTime())) {
+        query.date = { $gte: start, $lte: end };
+      }
+    }
+
+    console.log("*", req.query);
+
+    // const insertTransactions = await Transactions.insertMany(
+    //   transactionsData.transactions
+    // );
+
+    const getAllTransactions = await Transactions.find(query);
     res.send({
+      // insertTransactions,
       getAllTransactions,
     });
   })
 );
 
-//edit transaction
-//should user be able to update accountNumber or IDNumber?
-transactionsRouter.post(
+//get one transaction
+transactionsRouter.get(
   "/:id",
   expressAsyncHandler(async (req, res) => {
-    const transaction = await Transactions.findById(req.params.id);
+    const transaction = await Transactions.findOne({ id: req.params.id });
+
+    if (transaction) {
+      res.send(transaction);
+    } else {
+      res.status(404).send({ message: "Transaction not found." });
+    }
+  })
+);
+
+//add transaction
+transactionsRouter.post(
+  "/",
+  expressAsyncHandler(async (req, res) => {
+    const newTransaction = new Transactions({
+      date: Date.now(),
+      sender: {
+        firstName: "first name",
+        lastName: "last name",
+        dateOfBirth: "1900-09-09",
+        IDNumber: "100000",
+      },
+      recipient: {
+        firstName: "first name",
+        lastName: "last name",
+        email: "email",
+        accountNumber: "100000",
+        bank: "bank",
+      },
+      status: "status",
+    });
+    const addTransaction = await newTransaction.save();
+    res.send({ message: "Transaction created.", addTransaction });
+  })
+);
+
+//edit transaction
+//should user be able to update accountNumber or IDNumber?
+transactionsRouter.put(
+  "/:id",
+  expressAsyncHandler(async (req, res) => {
+    const transaction = await Transactions.findById(req.params._id);
 
     if (transaction) {
       transaction.date = req.body.date;
@@ -42,7 +110,7 @@ transactionsRouter.post(
       await transaction.save();
       res.send({ message: "Transaction successfully updated." });
     } else {
-      res.status(404).send({ message: "Transaction not found." });
+      res.status(404).send({ message: "Cannot update transaction." });
     }
   })
 );
@@ -51,13 +119,13 @@ transactionsRouter.post(
 transactionsRouter.delete(
   "/:id",
   expressAsyncHandler(async (req, res) => {
-    const transaction = await Transaction.findById(req.params.id);
+    const transaction = await Transactions.findById(req.params._id);
 
     if (transaction) {
       await transaction.remove();
       res.send({ message: "Transaction deleted." });
     } else {
-      res.status(400).send({ message: "Transaction not found." });
+      res.status(400).send({ message: "Cannot delete transaction." });
     }
   })
 );
